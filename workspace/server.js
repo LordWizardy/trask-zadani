e8aac4b177f30ada5f952e6a38174c307dd0443e
LordWizardy <l.wizardy@gmail.com>
2018-05-30 12:54:47 +0200

    Update server.js

--- workspace/server.js
+++ workspace/server.js
@@ -7,64 +7,73 @@ var fs = require("fs");
 
 var idNum = 0;
 
+// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Process application/json
 app.use(bodyParser.json());
 
-app.post('/myAction', function(req, res){  
 
-  idNum += 1;
-  
-  req.body.id = parseInt(req.body.id, 10);
-  req.body.id += idNum;
+app.post('/myAction', function(req, res){  
+//now req.body will be populated with the object you sent
+
   var json = req.body;
   var print = JSON.stringify(json,null,4);
   
   console.log(print);
   
   
+  
   //Validation
-  fs.readFile("./client/js/trasktest1.json", (err, data) => {
-    if (err) throw err;
-    console.log("File red");
-    
-    var userSchema = data;
-    var user = req.body;
+  var form = JSON.parse(print);
+  
+  var data = fs.readFileSync("./client/js/trasktest1.json","utf8");
+  var jsonSchema = JSON.parse(data);
+  
+  var endPage = "";
+  
     
     var Ajv = require('ajv');
     var ajv = Ajv({allErrors: true});
-    var validate = ajv.compile(userSchema);
-    var valid = validate(user);
-    if (valid) {
-      console.log('User data is valid');
+    ajv.addMetaSchema(require('./node_modules/ajv/lib/refs/json-schema-draft-06.json'));
+    var valid = ajv.validate(jsonSchema, form);
+    
+    if (!valid) {
+      console.log(ajv.errors);
+      endPage = ('<html><body><h1>Invalid form input</h1><br><form><input type="button" value="Back home" onclick="window.location.href=\'https://trask-test-zadani-lordwizardy.c9users.io/index.html\'" /></form></body></html>');
+    } else {
       
       //Writing to file
+      idNum += 1;
   
-        var fileContent = print + ", ";
-        
-        fs.writeFile("./client/forms/formResult"+idNum+".json", fileContent, (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log("File has been created");
-        });
-        
-  
+      req.body.id = parseInt(req.body.id, 10);
+      req.body.id += idNum;
+      req.body.id = String(req.body.id);
+      json = req.body;
+      print = JSON.stringify(json,null,4);
       
-    } else {
-      console.log('User data is INVALID!');
-      console.log(validate.errors);
+      var fileContent = print;
+      
+      fs.writeFile("./client/forms/formResult"+idNum+".json", fileContent, "utf8", (err) => {
+          if (err) {
+              console.error(err);
+              return;
+          }
+          console.log("File has been created");
+      });
+      endPage = ('<html><body><h1>Form sent</h1><br><form><input type="button" value="Back home" onclick="window.location.href=\'https://trask-test-zadani-lordwizardy.c9users.io/index.html\'" /></form></body></html>');
+      
     }
-  });
-
-  res.send('<html><body><h1>Form sent</h1><br><form><input type="button" value="Back home" onclick="window.location.href=\'https://localhost/index.html\'" /></form></body></html>');
+  
+  res.send(endPage);
+  
+  // res.send('<html><body><h1>Form sent</h1><br><form><input type="button" value="Back hom" onclick="window.location.href=\'https://trask-test-zadani-lordwizardy.c9users.io/index.html\'" /></form></body></html>');
 
 });
 
 
 
-//Print forms
+//Zobrazování formulářů
 
 var formsCont = "";
 
@@ -81,13 +90,14 @@ app.post('/forms', function(req, res){
     files.forEach(file => {
       console.log(file);
       
-      var x = fs.readFileSync('./client/forms/' + file, {encoding: "utf8"});
+      var x = fs.readFileSync('./client/forms/' + file, "utf8");
       console.log(x);
       formsCont += x;
       
     });
     
     console.log(formsCont);
+    //res.end('<html><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"></head><body>' + formsCont + '</body></html>');
     res.end(formsCont);
   });
   
@@ -98,7 +108,7 @@ app.post('/forms', function(req, res){
 //Setting up server
 app.use('/', express.static(__dirname + '/client')); // ← adjust
 
-server.listen(3000, "0.0.0.0", function(){
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
